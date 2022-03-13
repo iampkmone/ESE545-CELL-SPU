bf64d08c53e4955bedc57f1723d00dd5f061b1b2
AhibAlghazali <ahib.alghazali@stonybrook.edu>
2022-03-13 18:30:28 -0400

    Made FX1 compilable, added tb for FX1, confirmed functional operation of ah

--- SimpleFixed1.sv
+++ SimpleFixed1.sv
@@ -1,5 +1,5 @@
 module SimpleFixed1(clk, reset, op, format, rt_addr, ra, rb, imm, reg_write, rt_wb, rt_addr_wb, reg_write_wb);
-	input			clk, reset
+	input			clk, reset;
 	
 	//RF/FWD Stage
 	input [0:10]	op;				//Decoded opcode, truncated based on format
@@ -10,72 +10,78 @@ module SimpleFixed1(clk, reset, op, format, rt_addr, ra, rb, imm, reg_write, rt_
 	input			reg_write;		//Will current instr write to RegTable
 	
 	//WB Stage
-	output [0:127]	rt_wb;			//Output value of Stage 3
-	output [0:6]	rt_addr_wb;		//Destination register for rt_wb
-	output			reg_write_wb;	//Will rt_wb write to RegTable
+	output logic [0:127]	rt_wb;			//Output value of Stage 3
+	output logic [0:6]		rt_addr_wb;		//Destination register for rt_wb
+	output logic			reg_write_wb;	//Will rt_wb write to RegTable
 	
 	//Internal Signals
 	logic [1:0][0:127]	rt_delay;			//Staging register for calculated values
 	logic [1:0][0:6]	rt_addr_delay;		//Destination register for rt_wb
 	logic [1:0]			reg_write_delay;	//Will rt_wb write to RegTable
 	
-	logic [6:0]			i, j				//7-bit counter for loops
-	logic [3:0]			temp				//Incrementing counter
+	logic [6:0]		i;				//7-bit counter for loops
 	
 	// TODO : Implement all instr
-	
-	always_comb begin		
-		rt_wb = rt_delay[1];
-		rt_addr_wb = rt_addr_delay[1];
-		reg_write_wb = reg_write_delay[1];
-		
-		if (format == 0 && op == 0) begin					//nop : No Operation (Execute)
-			rt_delay[0] = 0;
-			rt_addr_delay[0] = 0;
-			reg_write_delay[0] = 0;
-		end
-		else begin
-			rt_addr_delay[0] = rt_addr;
-			reg_write_delay[0] = reg_write;
-			if (format == 0) begin
-				case (op)
-					11'b00011001000 : begin					//ah : Add Halfword
-						for (i=0; i<8; i=i+1) begin
-							rt_delay[0][(i*16) +: 16] = $signed(ra[0][(i*16) +: 16]) + $signed(rb[0][(i*16) +: 16]);
-						end
-					end
-				endcase
-			end
-			//else if (format == 1) begin
-			//end
-			//else if (format == 2) begin
-			//end
-			//else if (format == 3) begin
-			//end
-			//else if (format == 4) begin
-			//end
-			//else if (format == 5) begin
-			//end
-			//else if (format == 6) begin
-			//end
-		end
-	end
-	
+
 	always_ff @(posedge clk) begin
 		if (reset == 1) begin
+			rt_wb = 0;
+			rt_addr_wb = 0;
+			reg_write_wb = 0;
 			rt_delay[1] <= 0;
 			rt_addr_delay[1] <= 0;
 			reg_write_delay[1] <= 0;
-			
 			rt_delay[0] <= 0;
 			rt_addr_delay[0] <= 0;
 			reg_write_delay[0] <= 0;
 		end
 		else begin
+			rt_wb <= rt_delay[1];
+			rt_addr_wb <= rt_addr_delay[1];
+			reg_write_wb <= reg_write_delay[1];
 			rt_delay[1] <= rt_delay[0];
 			rt_addr_delay[1] <= rt_addr_delay[0];
 			reg_write_delay[1] <= reg_write_delay[0];
+			
+			rt_wb = rt_delay[1];
+			rt_addr_wb = rt_addr_delay[1];
+			reg_write_wb = reg_write_delay[1];
+			
+			if (format == 0 && op == 0) begin					//nop : No Operation (Execute)
+				rt_delay[0] = 0;
+				rt_addr_delay[0] = 0;
+				reg_write_delay[0] = 0;
+			end
+			else begin
+				rt_addr_delay[0] = rt_addr;
+				reg_write_delay[0] = reg_write;
+				if (format == 0) begin
+					case (op)
+						11'b00011001000 : begin					//ah : Add Halfword
+							for (i=0; i<8; i=i+1) begin
+								rt_delay[0][(i*16) +: 16] = $signed(ra[(i*16) +: 16]) + $signed(rb[(i*16) +: 16]);
+							end
+						end
+						default begin
+							rt_delay[0] = 0;
+							rt_addr_delay[0] = 0;
+							reg_write_delay[0] = 0;
+						end
+					endcase
+				end
+				//else if (format == 1) begin
+				//end
+				//else if (format == 2) begin
+				//end
+				//else if (format == 3) begin
+				//end
+				//else if (format == 4) begin
+				//end
+				//else if (format == 5) begin
+				//end
+				//else if (format == 6) begin
+				//end
+			end
 		end
 	end
-	
 endmodule
