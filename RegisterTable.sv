f5e639e906b1b84833bb92ab70d09740d52d6853
AhibAlghazali <ahib.alghazali@stonybrook.edu>
2022-03-15 00:26:31 -0400

    Noticed bug in RegFile forwarding logic that only allowed one reg at a time to be forwarded. Forwarding seemingly works as intended now

--- RegisterTable.sv
+++ RegisterTable.sv
@@ -1,13 +1,13 @@
-module RegisterTable(clk, reset, instr_even, instr_odd, format_even, format_odd, ra_even, rb_even, rc_even, ra_odd, rb_odd,
+module RegisterTable(clk, reset, instr_even, instr_odd, ra_even, rb_even, rc_even, ra_odd, rb_odd,
 		rt_st_odd, rt_addr_even, rt_addr_odd, rt_even, rt_odd, reg_write_even, reg_write_odd);
-	input logic					clk, reset;
-	input logic[0:31]			instr_even, instr_odd;			//Instructions to read from decoder
-	input logic[2:0]				format_even, format_odd;		//Instruction formats to read, decoded
-	output logic[0:127]	ra_even, rb_even, rc_even, ra_odd, rb_odd, rt_st_odd;	//Set all possible register values regardless of format
+	input					clk, reset;
+	
+	input [0:31]			instr_even, instr_odd;			//Instructions to read from decoder
+	output logic [0:127]	ra_even, rb_even, rc_even, ra_odd, rb_odd, rt_st_odd;	//Set all possible register values regardless of format
 
-	input logic[0:6]				rt_addr_even, rt_addr_odd;		//Destination registers to write to
-	input logic[0:127]			rt_even, rt_odd;				//Values to write to destination registers
-	input logic					reg_write_even, reg_write_odd;	//1 if instr will write to rt, else 0
+	input [0:6]				rt_addr_even, rt_addr_odd;		//Destination registers to write to
+	input [0:127]			rt_even, rt_odd;				//Values to write to destination registers
+	input 					reg_write_even, reg_write_odd;	//1 if instr will write to rt, else 0
 
 	logic [0:127] registers [0:127];						//RegFile
 	logic [7:0] 			i;								//8-bit counter for reset loop
@@ -28,32 +28,33 @@ module RegisterTable(clk, reset, instr_even, instr_odd, format_even, format_odd,
 		if (reg_write_even == 1) begin
 			if (instr_even[25:31] == rt_addr_even)
 				rc_even = rt_even;
-			else if (instr_even[18:24] == rt_addr_even)
+			if (instr_even[18:24] == rt_addr_even)
 				ra_even = rt_even;
-			else if (instr_even[11:17] == rt_addr_even)
+			if (instr_even[11:17] == rt_addr_even)
 				rb_even = rt_even;
-			else if (instr_odd[25:31] == rt_addr_even)
+			if (instr_odd[25:31] == rt_addr_even)
 				rt_st_odd = rt_even;
-			else if (instr_odd[18:24] == rt_addr_even)
+			if (instr_odd[18:24] == rt_addr_even)
 				ra_odd = rt_even;
-			else if (instr_odd[11:17] == rt_addr_even)
+			if (instr_odd[11:17] == rt_addr_even)
 				rb_odd = rt_even;
 		end
 		if (reg_write_odd == 1) begin
 			if (instr_even[25:31] == rt_addr_odd)
 				rc_even = rt_odd;
-			else if (instr_even[18:24] == rt_addr_odd)
+			if (instr_even[18:24] == rt_addr_odd)
 				ra_even = rt_odd;
-			else if (instr_even[11:17] == rt_addr_odd)
+			if (instr_even[11:17] == rt_addr_odd)
 				rb_even = rt_odd;
-			else if (instr_odd[25:31] == rt_addr_odd)
+			if (instr_odd[25:31] == rt_addr_odd)
 				rt_st_odd = rt_odd;
-			else if (instr_odd[18:24] == rt_addr_odd)
+			if (instr_odd[18:24] == rt_addr_odd)
 				ra_odd = rt_odd;
-			else if (instr_odd[11:17] == rt_addr_odd)
+			if (instr_odd[11:17] == rt_addr_odd)
 				rb_odd = rt_odd;
 		end
 	end
+	
 	always_ff @(posedge clk) begin
 		if(reset == 1)begin
 			registers[127] = 0;
@@ -68,5 +69,4 @@ module RegisterTable(clk, reset, instr_even, instr_odd, format_even, format_odd,
 				registers[rt_addr_odd] <= rt_odd;
 		end
 	end
-
 endmodule
