48ad787e607d9f92e70e862b4ee3a8437b3b0a93
AhibAlghazali <ahib.alghazali@stonybrook.edu>
2022-04-26 06:43:05 -0400

    Added program used for testing. Please confirm results of line 13, adding -1 to r25. Seems to produce incorrect result

---
+++ compiler/RAW_STALL_1.asm
@@ -0,0 +1,17 @@
+mpy 17 19 11
+lqd 4 5(10)
+mpy 80 11 5
+shlqby 1 80 8
+ai 33 16 31
+shlqby 34 44 55
+shl 80 11 5
+shlqby 1 81 8
+shl 89 11 5
+shlqby 1 80 8
+ila 25 2
+ila 26 0
+ai 25 25 -1
+ai 26 26 1
+brnz 25 14
+lnop
+stop
